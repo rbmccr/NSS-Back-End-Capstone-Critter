cf7e0c2d22e77150846d2fafe4cb5c7fa3b7b523
Brendan McCray <rbmccr@gmail.com>
2019-02-23 15:29:43 -0600

    wrote models for basic functionality and handled some file cleanup. Added some content to readme for later use when writing app

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Installing the project
 - Create an empty directory to house the project
-- run `virtualenv env` within that directory to create a virtual environment 
+- run `virtualenv env` within that directory to create a virtual environment
 - run `source env/bin/activate` to initialize a virtual environment (type `deactivate` at any time to exit the environment)
 - run `git clone [repository id]`
 - run `cd main`
@@ -12,3 +12,8 @@
 I've already created a file to populate the database.
 - run `python manage.py makemigrations api`
 - run `python manage.py migrate`
+
+### Adoption Applications
+- A user can submit only one adoption application per animal. Additionally, if a shelter staff member rejects the application, then the user who submitted the application will still not be able to submit another.
+- If an application is approved for a particular animal, then:
+-- Remaining applications in the system for that animal will not be visible by shelter staff members
--- app/models.py
+++ app/models.py
@@ -1,3 +1,97 @@
+from django.contrib.auth.models import User
+# Used with integerfield to restrict max value
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Volunteer(models.Model):
+    """Defines a model for a volunteer (a verified user). Volunteers who are admin are treated as staff.
+
+        Returns: __str__ userId, street_address, and phone_number
+    """
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=75)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=12)
+    phone_number = models.PositiveIntegerField(validators=[MaxValueValidator(999999999)])
+    delete_date = models.DateTimeField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return f"User: {self.user} Street Address:{self.street_address} Phone: {self.phone_number}"
+
+
+class Species(models.Model):
+    """Defines an animal's species (e.g. cat, dog).
+
+       Returns __str__ type
+    """
+
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return f"Species: {self.name}"
+
+
+class Breed(models.Model):
+    """Defines an animal's breed (e.g. domestic short hair).
+
+       Returns __str__ breed
+    """
+
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return f"Breed: {self.name}"
+
+
+class Color(models.Model):
+    """Defines an animal's color.
+
+       Returns __str__ color
+    """
+
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"Color: {self.name}"
+
+
+class Animal(models.Model):
+    """Defines an animal at the animal shelter.
+
+       Returns: __str__
+    """
+
+    name = models.CharField(max_length=75)
+    # you never know if you're going to adopt out a sea turtle...
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(200)])
+    species = models.ForeignKey(Species, on_delete=models.PROTECT)
+    breed = models.ForeignKey(Species, on_delete=models.PROTECT)
+    color = models.ForeignKey(Species, on_delete=models.PROTECT)
+    SEX_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
+    description = models.CharField(max_length=500)
+    date_arrival = models.DateTimeField(default=None, null=True, blank=True)
+    date_adopted = models.DateTimeField(default=None, null=True, blank=True)
+    staff = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Name: {self.name} Age: {self.age} Species: {self.species} Sex: {self.sex}"
+
+
+class Application(models.Model):
+    """Defines an application submitted by a user to request the adoption of an animal.
+
+        Returns: __str__ userId, date_submitted
+    """
+
+    date_submitted = models.DateTimeField(default=None, null=True, blank=True)
+    text = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"User: {self.user} Date Submitted: {self.date_submitted}"
