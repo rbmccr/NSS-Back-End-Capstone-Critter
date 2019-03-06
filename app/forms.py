7b3ef9aa8a9a47c0b0acf0bf6bc15d6baa771514
Brendan McCray <rbmccr@gmail.com>
2019-03-06 12:10:41 -0600

    user can now edit profile (need to initiate volunteer table edit also)

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,7 @@ class UserForm(forms.ModelForm):
     """
         This class is used in registration to define first_name, last_name, email, and password.
         The __init__ method has been modified in order to display crispy forms in a specific way.
+        Includes password confirmation.
     """
     password = forms.CharField(widget=forms.PasswordInput(), label='Password (at least 8 characters)')
     confirm_password=forms.CharField(widget=forms.PasswordInput())
@@ -53,21 +54,26 @@ class UserForm(forms.ModelForm):
 
 class EditProfileUserForm(forms.ModelForm):
     """
-        This class allows a user to edit their first_name, last_name, and email.
+        This class allows a user to edit their first_name, last_name, and email from their profile page.
         The __init__ method has been modified in order to display crispy forms in a specific way.
     """
-    password = forms.CharField(widget=forms.PasswordInput(), label='Password (at least 8 characters)')
-    confirm_email=forms.CharField()
 
-    def clean(self):
-        cleaned_data = super(EditProfileUserForm, self).clean()
-        email = cleaned_data.get('password')
-        confirm_email = cleaned_data.get('confirm_email')
+    # def clean_email(self):
+    #     cleaned_data = super(EditProfileUserForm, self).clean()
+    #     my_email = cleaned_data.get('email')
+    #     my_current_instance_id = CustomUser.objects.get(email=my_email).id
+    #     users = CustomUser.objects.all()
 
-        if email != confirm_email:
-            raise forms.ValidationError('')
+    #     for user in users:
+    #         if my_email == user.email:
+    #             user_email_owner_id = CustomUser.objects.get(email=user.email).id
+    #             print("$$$$$$$$$$$$$$$ user email owner id", user_email_owner_id, my_current_instance_id)
+    #             if my_current_instance_id == user_email_owner_id:
+    #                 pass
+    #             else:
+    #                 raise forms.ValidationError('')
 
-        return cleaned_data
+        # return cleaned_data['email']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -80,9 +86,7 @@ class EditProfileUserForm(forms.ModelForm):
                 css_class='form-row mb-n2'
             ),
             Row(
-                Column('email', css_class='form-group col-md-6 mb-0'),
-                Column('confirm_email', css_class='form-group col-md-6 mb-0'),
-                css_class='form-row mb-n2'
+                Column('email', css_class='form-group col-md-6 mb-0')
             ),
         )
 
@@ -92,6 +96,34 @@ class EditProfileUserForm(forms.ModelForm):
 
 
 class VolunteerForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column('phone_number', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row mb-n2'
+            ),
+            Row(
+                Column('street_address', css_class='form-group col-md-12 mb-0'),
+                css_class='form-row mb-n2'
+            ),
+            Row(
+                Column('city', css_class='form-group col-md-6 mb-0'),
+                Column('state', css_class='form-group col-md-4 mb-0'),
+                Column('zipcode', css_class='form-group col-md-2 mb-0'),
+                css_class='form-row mb-n2'
+            ),
+        )
+
+    class Meta:
+        model = Volunteer
+        fields = ('phone_number', 'street_address', 'city', 'state', 'zipcode',)
+
+
+class EditProfileVolunteerForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- app/views/profile.py
+++ app/views/profile.py
@@ -1,23 +1,83 @@
-from django.contrib.auth import logout, login, authenticate
+# authentication
+from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+# HTTP
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import RequestContext
-from django.contrib.auth.models import User
-from app.models import Application
-from app.forms import UserForm, VolunteerForm, EditProfileUserForm
+from django.urls import reverse
+# messages
+from django.contrib import messages
+# models
+from app.models import CustomUser, Application
+# forms
+from app.forms import UserForm, VolunteerForm, EditProfileUserForm, EditProfileVolunteerForm
 
 @login_required
 def edit_profile(request):
+    """
+        This view function captures the current user's contact information and renders forms to edit the data.
+        On POST, the user instance and volunteer instance are updated in the database.
+    """
 
     if request.method == 'GET':
         user = request.user
-        edit_user_form = EditProfileUserForm()
+
+        user_data = {
+            'first_name':user.first_name,
+            'last_name':user.last_name,
+            'email': user.email
+        }
+
+        volunteer_data = {
+            'phone_number':user.volunteer.phone_number,
+            'street_address':user.volunteer.street_address,
+            'city': user.volunteer.city,
+            'state': user.volunteer.state,
+            'zipcode': user.volunteer.zipcode
+        }
+
+        edit_user_form = EditProfileUserForm(initial=user_data)
+        edit_volunteer_form = EditProfileVolunteerForm(initial=volunteer_data)
+
         context = {
             'user': user,
             'edit_user_form': edit_user_form,
+            'edit_volunteer_form': edit_volunteer_form,
         }
+
         return render(request, 'app/edit_profile.html', context)
+
+    if request.method == 'POST':
+        # get user instance to provide to form class
+        user = CustomUser.objects.get(pk=request.user.id)
+        edit_user_form = EditProfileUserForm(data=request.POST, instance=user)
+        edit_volunteer_form = EditProfileVolunteerForm(data=request.POST)
+
+        # check data types in submission. Django also checks to ensure username (e-mail in this project) is unique.
+        if edit_user_form.is_valid() and edit_volunteer_form.is_valid():
+            # Get user instance and update the user's data
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.email = request.POST['email']
+            user.save()
+
+            # volunteer = edit_volunteer_form.save(commit=False)
+            # volunteer.user = user
+            # volunteer.save()
+
+            # return to user profile
+            return HttpResponseRedirect(reverse('app:profile'))
+
+        else:
+            # re-populate form with submitted data. Include message
+            context = {
+                'edit_user_form': edit_user_form,
+                'edit_volunteer_form': edit_volunteer_form
+            }
+
+            messages.error(request, "Update failed.")
+            return render(request, 'app/edit_profile.html', context)
 
 @login_required
 def profile(request):
