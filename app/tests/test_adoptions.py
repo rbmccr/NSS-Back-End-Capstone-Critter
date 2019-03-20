50b3fabca5f4d132fc5839fe543bbb33cc135d2f
Brendan McCray <rbmccr@gmail.com>
2019-03-20 14:28:08 -0500

    achieved 68% testing coverage and removed one of five naive datetime errors

--- app/tests/test_adoptions.py
+++ app/tests/test_adoptions.py
@@ -9,7 +9,8 @@ from app.models import Animal, Application, Breed, Color, CustomUser, Species
 from app.forms import AnimalForm
 # tools
 import datetime
-
+from django.conf import settings
+from django.utils.timezone import make_aware
 
 class AdminAdoptionsTest(TestCase):
     """
@@ -36,6 +37,10 @@ class AdminAdoptionsTest(TestCase):
             test_unauth_user_cannot_view_specific_applications
             test_HTML_content_of_specific_applications_page
             test_staff_user_can_view_approval_and_cancellation_pages
+            test_auth_user_cannot_view_approval_and_cancellation_pages
+            test_unauth_user_cannot_view_approval_and_cancellation_pages
+            test_posting_to_rejection_applies_False_condition_to_application
+            test_posting_to_approval_applies_True_condition_to_application
     """
 
     @classmethod
@@ -98,7 +103,7 @@ class AdminAdoptionsTest(TestCase):
             age = '2018-03-18',
             sex = 'M',
             description = 'This is the pet\'s description.',
-            date_adopted = '2019-03-20',
+            date_adopted = make_aware(datetime.datetime(2019, 3, 20, 00, 0, 00, 000000)),
             breed = breed,
             color = color,
             species = species,
@@ -115,7 +120,7 @@ class AdminAdoptionsTest(TestCase):
             reason = None,
             animal = unadopted_animal,
             user = auth_user,
-            date_submitted = datetime.datetime.now()
+            date_submitted = make_aware(datetime.datetime.now())
         )
 
     def test_staff_can_view_list_of_animals(self):
@@ -259,3 +264,69 @@ class AdminAdoptionsTest(TestCase):
         self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('app:reject_application', args=(1,1,)))
         self.assertEqual(response.status_code, 302)
+
+    def test_posting_to_rejection_applies_False_condition_to_application(self):
+        """
+            Validate that POSTing to reject url path applies a 0 (False) to submitted application.
+            Then validate that the pending application no longer appears in the list_applications view
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        form_data = {'reason': 'not a good reason',}
+
+        # post to rejection url path
+        response = self.client.post(reverse('app:reject_application', args=(1,1,)), form_data)
+        # get application and check approved property
+        application = Application.objects.get(pk=1)
+        self.assertFalse(application.approved)
+        # get list of animals
+        response = self.client.get(reverse('app:list_applications'))
+        # confirm pending application does not appear
+        self.assertNotIn('1 pending</span>'.encode(), response.content)
+
+    def test_posting_to_approval_applies_True_condition_to_application(self):
+        """
+            Validate that POSTing to approval url path applies a 1 (True) to submitted application.
+            Then validate that the animal no longer appears in the list_applications view
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        # post to final_decision url path
+        response = self.client.post(reverse('app:final_decision', args=(1,1,)))
+        # get application and check approved property
+        application = Application.objects.get(pk=1)
+        self.assertTrue(application.approved)
+        # get list of animals
+        response = self.client.get(reverse('app:list_applications'))
+        # confirm success message and conditional statement return that only appears when no unadopted animals are returned
+        self.assertIn('approved the adoption of test_animal_unadopted'.encode(), response.content)
+        self.assertIn('No unadopted animals were returned from the database'.encode(), response.content)
+
+    def test_staff_user_can_revise_judgment(self):
+        """
+            Validate that a staff user can remove the rejected status of an application
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        form_data = {'reason': 'not a good reason',}
+
+        # post to final_decision url path
+        response = self.client.post(reverse('app:reject_application', args=(1,1,)), form_data)
+        # get application and check approved property
+        application = Application.objects.get(pk=1)
+        self.assertFalse(application.approved)
+        # get specific animal's application list
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        # confirm pending application does not appear
+        self.assertNotIn('1 pending</span>'.encode(), response.content)
+        # revise judgment of the application
+        response = self.client.get(reverse('app:revise_judgment', args=(1,1,)))
+        application = Application.objects.get(pk=1)
+        # confirm approved property is None
+        self.assertEqual(application.approved, None)
+        # confirm pending application now appears
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        self.assertIn('1 pending</span>'.encode(), response.content)
