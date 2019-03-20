2f1ab2bef17544e79d722ad2645517328a307495
Brendan McCray <rbmccr@gmail.com>
2019-03-20 13:41:04 -0500

    updated requirements txt and also achieved 64% test coverage

--- app/tests/test_adoptions.py
+++ app/tests/test_adoptions.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 from app.models import Animal, Application, Breed, Color, CustomUser, Species
 # forms
 from app.forms import AnimalForm
+# tools
+import datetime
 
 
 class AdminAdoptionsTest(TestCase):
@@ -27,7 +29,13 @@ class AdminAdoptionsTest(TestCase):
             test_staff_can_view_list_of_animals
             test_auth_user_cannot_view_list_of_animals
             test_unauth_user_cannot_view_list_of_animals
-            test_HTML_content_of_list
+            test_unadopted_vs_adopted_in_list
+            test_pending_notification_in_list
+            test_staff_user_can_view_specific_applications
+            test_auth_user_cannot_view_specific_applications
+            test_unauth_user_cannot_view_specific_applications
+            test_HTML_content_of_specific_applications_page
+            test_staff_user_can_view_approval_and_cancellation_pages
     """
 
     @classmethod
@@ -47,7 +55,7 @@ class AdminAdoptionsTest(TestCase):
         )
 
         # create user (not an administrator)
-        CustomUser.objects.create_user(
+        auth_user = CustomUser.objects.create_user(
             # id = 2
             first_name='Test_firstname2',
             last_name='Test_lastname2',
@@ -70,7 +78,7 @@ class AdminAdoptionsTest(TestCase):
         )
 
         # create animal instance [UNADOPTED]
-        Animal.objects.create(
+        unadopted_animal = Animal.objects.create(
             name = 'test_animal_unadopted',
             age = '2018-03-17',
             sex = 'F',
@@ -97,6 +105,17 @@ class AdminAdoptionsTest(TestCase):
             staff = staff,
             arrival_date = '2019-03-17',
             # note no image provided
+        )
+
+        # create pending application for the unadopted animal
+        Application.objects.create(
+            text = 'I want to adopt this animal.',
+            staff = staff,
+            approved = None,
+            reason = None,
+            animal = unadopted_animal,
+            user = auth_user,
+            date_submitted = datetime.datetime.now()
         )
 
     def test_staff_can_view_list_of_animals(self):
@@ -130,7 +149,7 @@ class AdminAdoptionsTest(TestCase):
         response = self.client.get(reverse('app:list_applications'))
         self.assertEqual(response.status_code, 302)
 
-    def test_HTML_content_of_list(self):
+    def test_unadopted_vs_adopted_in_list(self):
         """
             Validate that a staff user can see the current unadopted animal in the list and cannot see the adopted animal
         """
@@ -142,3 +161,101 @@ class AdminAdoptionsTest(TestCase):
         self.assertIn('test_animal_unadopted'.encode(), response.content)
         # confirm adopted animal's name does not appear
         self.assertNotIn('test_animal_adopted'.encode(), response.content)
+
+    def test_pending_notification_in_list(self):
+        """
+            Validate that an animal will have a tag with a pending notification if there is an un-rejected adoption application
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        response = self.client.get(reverse('app:list_applications'))
+        # confirm unadopted animal's name appears
+        self.assertIn('1 pending</span>'.encode(), response.content)
+        # delete application, reload page, and validate that pending does not appear
+        application = Application.objects.get(pk=1)
+        application.delete()
+        response = self.client.get(reverse('app:list_applications'))
+        self.assertNotIn('1 pending</span>'.encode(), response.content)
+
+    def test_staff_user_can_view_specific_applications(self):
+        """
+            Validate that a staff user can see a specific animal's applications
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        # confirm that staff user gets 200
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_auth_user_cannot_view_specific_applications(self):
+        """
+            Validate that an auth user cannot see a specific animal's applications
+        """
+
+        self.client.login(email='test_auth@test.com', password='secret2')
+
+        # confirm that auth user gets 302
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        self.assertEqual(response.status_code, 302)
+
+    def test_unauth_user_cannot_view_specific_applications(self):
+        """
+            Validate that an unauth user cannot see a specific animal's applications
+        """
+
+        # confirm that unauth user gets 302
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        self.assertEqual(response.status_code, 302)
+
+    def test_HTML_content_of_specific_applications_page(self):
+        """
+            Confirm expected content appears on specific applications view
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        response = self.client.get(reverse('app:list_specific_applications', args=(1,)))
+        # confirm unadopted animal's name appears
+        self.assertIn('1 pending</span>'.encode(), response.content)
+        self.assertIn('You\'re Viewing: test_animal_unadopted'.encode(), response.content)
+        self.assertIn('Test_lastname2, Test_firstname2'.encode(), response.content)
+        self.assertIn('I want to adopt this animal.'.encode(), response.content)
+
+    def test_staff_user_can_view_approval_and_cancellation_pages(self):
+        """
+            Validate that a staff user can view the approval and cancellation pages
+        """
+
+        self.client.login(email='test_admin@test.com', password='secret')
+
+        # confirm that staff user gets 200
+        response = self.client.get(reverse('app:final_decision', args=(1,1,)))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('app:reject_application', args=(1,1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_auth_user_cannot_view_approval_and_cancellation_pages(self):
+        """
+            Validate that an auth user cannot view the approval and cancellation pages
+        """
+
+        self.client.login(email='test_auth@test.com', password='secret2')
+
+        # confirm that auth user gets 302
+        response = self.client.get(reverse('app:final_decision', args=(1,1,)))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('app:reject_application', args=(1,1,)))
+        self.assertEqual(response.status_code, 302)
+
+    def test_unauth_user_cannot_view_approval_and_cancellation_pages(self):
+        """
+            Validate that an unauth user cannot view the approval and cancellation pages
+        """
+
+        # confirm that unauth user gets 302
+        response = self.client.get(reverse('app:final_decision', args=(1,1,)))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('app:reject_application', args=(1,1,)))
+        self.assertEqual(response.status_code, 302)
